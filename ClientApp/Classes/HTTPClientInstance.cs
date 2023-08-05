633e5854f46245e90a72a2c717ca65558a8fc0fe
19g9nd <ahadova2017@gmail.com>
2023-08-05 13:46:39 +0400

    add checks,reg page hhtp,singleton

---
+++ ClientApp/Classes/HTTPClientInstance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Threading;
+
+namespace ClientApp.Classes
+{
+    public class HTTPClientInstance
+    {
+        private static readonly HttpClient httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost"),
+            Timeout = TimeSpan.FromSeconds(30)
+            //если сервер не отвечает в течении 30 секунд то сервер генерирует исключение(избежание зависания)
+        };
+        public static HttpClient Instance => httpClient;
+        //доступ к одному и тому же экземпляру HttpClient
+    }
+}
--- ClientApp/RegisterWindow.xaml.cs
+++ ClientApp/RegisterWindow.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Azure;
+using ClientApp.Classes;
+using Shared.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,19 +20,56 @@ using System.Windows.Shapes;
 
 namespace ClientApp
 {
-    /// <summary>
-    /// Interaction logic for RegisterWindow.xaml
-    /// </summary>
     public partial class RegisterWindow : Window
     {
+
+        private readonly HttpClient httpClient = HTTPClientInstance.Instance;
         public RegisterWindow()
         {
             InitializeComponent();
         }
 
-        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)//async buttonclick?
         {
+            // Проверка полей на пустоту
+            if (string.IsNullOrWhiteSpace(NameTextBox.Text) || string.IsNullOrWhiteSpace(SurnameTextBox.Text) ||
+                string.IsNullOrWhiteSpace(PasswordBox.Password) || string.IsNullOrWhiteSpace(EmailTextBox.Text) ||
+                string.IsNullOrWhiteSpace(AdressTextBox.Text) || string.IsNullOrWhiteSpace(AgeTextBox.Text) ||
+                string.IsNullOrWhiteSpace(GenderTextBox.Text))
+            {
+                MessageTextBlock.Text = "Fill all empty boxes!";
+                return;
+            }
+            // Проверка возраста на числовое значение
+            if (!int.TryParse(AgeTextBox.Text, out int age))
+            {
+                MessageTextBlock.Text = "Non correct age!";
+                return;
+            }
 
+            Student newStudent = new Student
+            {
+                name = NameTextBox.Text,
+                surname = SurnameTextBox.Text,
+                adress = AdressTextBox.Text,
+                email = EmailTextBox.Text,
+                age = int.Parse(AgeTextBox.Text),
+                gender = GenderTextBox.Text
+            };
+           
+            HttpContent content =JsonContent.Create(newStudent);
+            HttpResponseMessage response = httpClient.PostAsync("http://localhost/students/create", content).Result;//await
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Student registered succesfully!");
+                // переход на другую страницу после успешной регистрации.
+            }
+            else
+            {
+                MessageTextBlock.Text = "Register error.";
+            }
         }
+
+      
     }
 }
